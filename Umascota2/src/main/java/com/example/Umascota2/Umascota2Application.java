2e0a7826829f98c685eb5be9201aae2499a484ee
FrescoDev <silbacrack@gmail.com>
2025-09-17 19:26:05 -0500

    Add files via upload

---
+++ Umascota2/src/main/java/com/example/Umascota2/Umascota2Application.java
@@ -0,0 +1,13 @@
+package com.example.Umascota2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Umascota2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Umascota2Application.class, args);
+	}
+
+}
---
+++ Umascota2/src/test/java/com/example/Umascota2/Umascota2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Umascota2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Umascota2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
